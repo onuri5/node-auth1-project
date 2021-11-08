402993e049302515042aeb4f57ff7dbcffb9d2b2
onuri5 <jomibaofficial@gmail.com>
2021-11-08 14:19:43 -0600

    finished all auth middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,49 +1,50 @@
-/*
-  If the user does not have a session saved in the server
+const Users = require("../users/users-model");
 
-  status 401
-  {
-    "message": "You shall not pass!"
+module.exports = { 
+  restricted,
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree
+};
+
+function restricted(req, res, next) {
+  if (req.sessions.user) {
+    next();
+  } else {
+    next({ status: 401, message: "You shall not pass!" });
   }
-*/
-function restricted() {
-
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+  Users.find()
+    .then((usersArr) => {
+      const free = usersArr.filter(
+        (username) => username === req.body.username
+      );
+      if (!free) {
+        next({ status: 401, message: "Username taken" });
+      } else {
+        next();
+      }
+    })
+    .catch(next);
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
+async function checkUsernameExists(req, res, next) {
+  const { username } = req.body;
+  const [ user ] = await Users.findBy({ username });
+  if (!user) {
+    next({ status: 401, message: "Invalid credentials" });
+  } else {
+    next();
   }
-*/
-function checkUsernameExists() {
-
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+function checkPasswordLength(req, res, next) {
+  const { password } = req.body;
+  if (password === undefined || password.length <= 3) {
+    next({ status: 422, message: "Password must be longer than 3 chars" });
+  } else {
+    next();
   }
-*/
-function checkPasswordLength() {
-
 }
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
